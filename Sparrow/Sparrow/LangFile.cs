cfe85b04ebde4ad9d294bbc34295293f4a3a0dee
Pipe <pipernene@gmail.com>
2015-12-24 17:26:07 +0100

    Added Load method and Entries dictionary

--- Sparrow/Sparrow/LangFile.cs
+++ Sparrow/Sparrow/LangFile.cs
@@ -3,10 +3,17 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.IO;
 
 namespace Sparrow
 {
     class LangFile
     {
+        private Dictionary<string, string> Entries;
+
+        public bool Load(string file)
+        {
+
+        }
     }
 }
