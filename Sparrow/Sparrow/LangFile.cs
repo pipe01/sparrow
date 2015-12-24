3197d6af935a6a5a2426920ab73a199770a359f8
Pipe <pipernene@gmail.com>
2015-12-24 18:01:43 +0100

    Added a test argument to Load to choose to not apply the file, header checking

--- Sparrow/Sparrow/LangFile.cs
+++ Sparrow/Sparrow/LangFile.cs
@@ -9,27 +9,38 @@ namespace Sparrow
 {
     public class LangFile
     {
+        private const string LangFileHeader = "Sparrow Language File v0.0.1";
+
         protected internal Dictionary<string, string> Entries;
 
-        public string[] AvailableLanguages(string dir)
+        public static string[] AvailableLanguages(string dir)
         {
             if (!Directory.Exists(dir)) return null;
 
             List<string> l = new List<string>();
 
+            LangFile tester = new LangFile();
+
             foreach (string item in Directory.EnumerateFiles(dir, "*.lang"))
             {
-                l.Add(Path.GetFileNameWithoutExtension(item));
+                if (tester.Load(item, true))
+                    l.Add(Path.GetFileNameWithoutExtension(item));
             }
 
             return l.ToArray();
         }
 
-        public bool Load(string lang)
+        public bool Load(string lang, bool test = false)
         {
             if (!File.Exists(lang + ".lang")) return false;
 
-            string[] lines = File.ReadAllLines(lang + ".lang");
+            string[] linesTmp = File.ReadAllLines(lang + ".lang");
+            string[] lines = new string[] { };
+
+            if (linesTmp[0] == LangFileHeader)
+                linesTmp.CopyTo(lines, 1);
+            else
+                return false;
 
             foreach (string l in lines)
             {
@@ -40,7 +51,7 @@ namespace Sparrow
                 string key = l.Substring(0, ePos);
                 string val = l.Substring(ePos, l.Length - ePos);
 
-                Entries.Add(key, val);
+                if (!test) Entries.Add(key, val);
             }
 
             return true;
